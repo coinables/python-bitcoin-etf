b98e9ab71927cf2f2c77df054b16e588fa932253
Kyle <coinables@gmail.com>
2017-02-20 17:38:04 -0800

    python2

--- etf.py
+++ etf.py
@@ -1,15 +1,15 @@
-import urllib.request
+import urllib2
 import threading
  
 def checkETF():
-    threading.Timer(90.0, checkETF).start() #set to 90 second refresh rate
+    threading.Timer(90.0, checkETF).start()
     target_url = "https://www.sec.gov/rules/sro/batsbzx.htm"
-    data = urllib.request.urlopen(target_url).read(20000) #set to only check first 20000 chars
+    data = urllib2.urlopen(target_url).read(20000) #set to only check first 20000 chars
     eachWord = data.split()
  
     if "Bitcoin" in eachWord:
-        print("THERES AN UPDATE ON THE BITCOIN ETF!")
+        print "Found"
     else:
-        print("Nothing")
+        print "Nothing"
  
 checkETF()
