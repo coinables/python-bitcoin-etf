0ef3f3c3f74542ead52b8b2dbef4c6f625de0013
Kyle Honeycutt <coinables@gmail.com>
2017-03-09 18:40:20 -0800

    added comments
    added comments

--- etf.py
+++ etf.py
@@ -1,15 +1,23 @@
+#urllib needed to fetch urls and threading needed for timer
 import urllib2
 import threading
- 
+
+#create a function that starts the timer and checks the page
 def checkETF():
+    #initiate timer for every 90 seconds
     threading.Timer(90.0, checkETF).start()
+    #define the page we want to check
     target_url = "https://www.sec.gov/rules/sro/batsbzx.htm"
+    #call urllib to the access the target_url then add the read property to limit how many characters
     data = urllib2.urlopen(target_url).read(20000) #set to only check first 20000 chars
+    #take every word and put it into a list
     eachWord = data.split()
- 
+    
+    #if operation to compare each word in the list to "bitcoin"
     if "Bitcoin" in eachWord:
         print "Found"
     else:
         print "Nothing"
- 
+      
+#run the function 
 checkETF()
